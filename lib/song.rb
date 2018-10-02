0ff0cfba23ca67b31cc3071536f4879669390b60
Sean Martin <sean.martin@gmail.com>
2018-10-02 03:24:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,7 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    if !@@genres.include?(genre)
-      @@genres << genre
-    end
+    @@genres << genre
   end
 
   def self.count
@@ -19,7 +17,7 @@ class Song
   end
 
   def self.genre
-    @@genres
+    @@genres.unique
   end
 end
 
