b4ebdddf8094e53e85ab0f8fcf42ca52db7d7632
Sean Martin <sean.martin@gmail.com>
2018-10-02 03:30:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,11 +19,11 @@ class Song
   end
 
   def self.genre
-    @@genres.unique
+    @@genres.uniq
   end
 
   def self.artists
-    @@artists.unique
+    @@artists.uniq
   end
 
   def self.genre_count
