0dbb7283ae4f600a0155d4fe3d2791781bb562f9
Sean Martin <sean.martin@gmail.com>
2018-10-02 03:21:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,13 @@
 class Song
+  @@count = 0
+
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
   end
 end
 
